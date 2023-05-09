3d87a3782073e4c9bb41fa4a9ce18f8c3a3e8408
belm960 <samuelbefikadu10@gmail.com>
2023-05-09 19:25:58 -0300

    some improvement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './Component/about/about.component';
+import { CalculateComponent } from './Component/calculate/calculate.component';
+import { FooterComponent } from './Component/footer/footer.component';
+import { HeaderComponent } from './Component/header/header.component';
+import { NewsComponent } from './Component/news/news.component';
+import { OfferComponent } from './Component/offer/offer.component';
+import { ReviewComponent } from './Component/review/review.component';
+import { ServicesComponent } from './Component/services/services.component';
+import { SliderComponent } from './Component/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    CalculateComponent,
+    FooterComponent,
+    HeaderComponent,
+    NewsComponent,
+    OfferComponent,
+    ReviewComponent,
+    ServicesComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
